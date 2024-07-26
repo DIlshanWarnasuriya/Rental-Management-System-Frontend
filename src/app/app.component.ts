75d2a4c27038e26cc54ca65f0ecb3854b64e66d5
Dilshan Warnasuriya <dilshan.se.dew@gmail.com>
2024-07-26 16:47:44 +0530

    Rental Page User interface Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from './component/navigation-bar/navigation-bar.component';
+import { RentalComponent } from './page/rental/rental.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationBarComponent, RentalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
