f7f9366d63cbdc3f3b77d13e0f2037b912f5ce4f
Dilshan Warnasuriya <dilshan.se.dew@gmail.com>
2024-08-03 00:53:19 +0530

    rental list view done .
    3 action buttons have . return, edit and delete

--- src/app/page/rental/rental.component.ts
+++ src/app/page/rental/rental.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rental',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './rental.component.html',
   styleUrl: './rental.component.css'
 })
-export class RentalComponent {
+export class RentalComponent implements OnInit{
+  
+  public rentalList: any = null;
+
+  ngOnInit(): void {
+    this.loadTable();
+  }
+
+  loadTable(){
+    fetch("http://localhost:8080/rental")
+    .then(res => res.json())
+    .then(data => {this.rentalList=data
+      this.datesFix();
+    }
+    )
+  }
+
+  datesFix(){
+    this.rentalList.forEach((element: any) => {
+      if(element.rentalDate != null){
+        element.rentalDate = new Date(element.rentalDate).toISOString().split('T')[0];
+      }
+
+      if(element.expectedDate != null){
+        element.expectedDate = new Date(element.expectedDate).toISOString().split('T')[0];
+      }
+
+      if(element.returnDate != null){
+        element.returnDate = new Date(element.returnDate).toISOString().split('T')[0];
+      }
+    });
+  }
+
+
 
 }
